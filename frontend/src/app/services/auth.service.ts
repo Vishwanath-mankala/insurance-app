d4da7d1d2bd3bae05f3450bf0d775b7cdff67c2e
Vishwanath <vishwanathmankala2@gmail.com>
2025-09-27 02:31:17 +0530

    feat: implement authentication flow with login and registration components, including styles and tests

--- frontend/src/app/services/auth.service.ts
+++ frontend/src/app/services/auth.service.ts
@@ -1,21 +1,70 @@
 import { Injectable } from '@angular/core';
-import { BehaviorSubject } from 'rxjs';
+import { BehaviorSubject, tap } from 'rxjs';
 import { User } from '../models/user';
+import { HttpClient } from '@angular/common/http';
+import { JwtHelperService } from '@auth0/angular-jwt';
+import { Router } from '@angular/router';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AuthService {
-  private userSubject = new BehaviorSubject<User | null>(null)
+  private userSubject = new BehaviorSubject<User | null>(null);
   user$ = this.userSubject.asObservable();
-  authUrl = 'http://localhost:4000/api/auth';
-  
-  register(name:String, email:String, password:String,role:String){
-    
+  authUrl = 'http://localhost:4000/api/v1/auth';
+  constructor(private http: HttpClient, private jwtHelper: JwtHelperService,private router:Router) {
+    if (this.checkLogin()) {
+      const token = localStorage.getItem('token');
+      if (token) {
+        const decodedToken = this.jwtHelper.decodeToken(token);
+        const user: User = {
+          id: decodedToken.userid,
+          name: decodedToken.name,
+          email: decodedToken.email,
+          role: decodedToken.role,
+        };
+        this.userSubject.next(user);
+        router.navigate(['/'])
+      }
+    }
   }
-  
-  login(){
 
+  checkLogin(): boolean {
+    const token = localStorage.getItem('token');
+    const loggedIn = !!token && !this.jwtHelper.isTokenExpired(token);
+
+    if (!loggedIn) {
+      this.userSubject.next(null); // clear user if token expired
+      localStorage.removeItem('token');
+    }
+
+    return loggedIn;
   }
-  constructor() { }
+  register(name: string, email: string, password: string, role: string) {
+    return this.http.post<User>(`${this.authUrl}/register`, {
+      name,
+      email,
+      password,
+      role,
+    });
+  }
+
+  login(email: string, password: string) {
+    return this.http
+      .post<{ token: string; user: User }>(`${this.authUrl}/login`, {
+        email,
+        password,
+      })
+      .pipe(
+        tap((response) => {
+          this.userSubject.next(response.user);
+          localStorage.setItem('token', response.token);
+        })
+      );
+  }
+
+  logout() {
+    this.userSubject.next(null);
+    localStorage.removeItem('token');
+  }
 }
