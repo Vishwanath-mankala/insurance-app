48cfe74652317be47d0b232ead402a4dce86f11f
Vishwanath <vishwanathmankala2@gmail.com>
2025-09-29 02:00:19 +0530

    feat: add landing page component with routing, styles, and HTML structure

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -3,5 +3,5 @@ import { Routes } from '@angular/router';
 export const routes: Routes = [
     { path: 'register', loadComponent: () => import('./pages/register/register.component').then(m => m.RegisterComponent) },
     { path: 'login', loadComponent: () => import('./pages/login/login.component').then(m => m.LoginComponent) },
-    { path: '', loadComponent: () => import('./pages/home/home.component').then(m => m.HomeComponent) },
+    { path: '', loadComponent: () => import('./pages/landing-page/landing-page.component').then(m => m.LandingPageComponent) },
 ];
